e79385b188adf313ccdfe7656d1870314627e2ae
lungnahahd <jyoungjk33@gmail.com>
2023-04-25 01:25:23 +0900

    Refactor : Test Code refactor Junit 활용 Test code 리팩토링

--- JavaNextStep/src/firststep/CalculatorTest.java
+++ JavaNextStep/src/firststep/CalculatorTest.java
@@ -1,34 +1,51 @@
 package firststep;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 // Production Code를 테스트 해보는 코드 
-// 메소드를 분리해서 각각을 테스트 할 수 있도록 구현  
+// 메소드를 분리해서 각각을 테스트 할 수 있도록 구현  -> Junit을 활용한 테스트 코드로 수정 
 public class CalculatorTest {
-	public static void main(String[] args)
+	private Calculator cal;
+	
+	@Before // 매 테스트마다 동작하는 것을 어노테이션으로 처리 -> 추후에 Junit에서 확장 가능성이 커짐!
+	public void setup()
 	{
-		Calculator cal = new Calculator();
-		add(cal);
-		substract(cal);
-		multiply(cal);
-		divide(cal);
+		cal = new Calculator();
+		System.out.println("Before");
 	}
 	
-	private static void add(Calculator cal)
+	@Test
+	public void add() 
 	{
-		System.out.println(cal.add(3,4));
+		assertEquals(9,cal.add(6, 3));
 	}
 	
-	private static void substract(Calculator cal)
+	@Test
+	public void subtract()
 	{
-		System.out.println(cal.subtract(5,4));
+		assertEquals(3, cal.subtract(6, 3));
 	}
 	
-	private static void multiply(Calculator cal)
+	@Test
+	public void multiply()
 	{
-		System.out.println(cal.multiply(2,6));
+		assertEquals(18, cal.multiply(6, 3));
 	}
 	
-	private static void divide(Calculator cal)
+	@Test
+	public void divide()
 	{
-		System.out.println(cal.divide(8,4));
+		assertEquals(2,cal.divide(6, 3));
 	}
+	
+	@After
+	public void afterwork()
+	{
+		System.out.println("End Test");
+	}
+	
 }
